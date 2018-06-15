13e59e652a9e65729630e96012ca9684d4de6d29
Bernadett Villam Lopiccolo <bernadettvillam@yahoo.com>
2018-06-15 14:25:43 -0700

    added pdf to tini waxing and some text to front page, not final or sized to other media

---
+++ assets/js/web.js
@@ -0,0 +1,24 @@
+// $(document).ready(function() {
+//  console.log("page is loaded");
+// var i = 0;
+// var txt = {'Ha gondot jelent az intim területek szőrtelenítése..'
+// 'Ha eleged van már a borotválás okozta kellemetlenségekből...' 'Ha ápolt, sima bőrre vágysz...' '....akkor itt az ideje, hogy legyen egy saját intimkozmetikusod!'};
+// var speed = 50;
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("text").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//     console.log("function is active");
+//   }
+// }
+// });
+
+
+
+// new TypeIt('#text', {
+//     strings: ['Ha gondot jelent az intim területek szőrtelenítése..', 'Ha eleged van már a borotválás okozta kellemetlenségekből...' 'Ha ápolt, sima bőrre vágysz...', '....akkor itt az ideje, hogy legyen egy saját intimkozmetikusod!'],
+//     speed:50,
+//     autoStart: false
+// });
